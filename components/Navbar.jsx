16cd7149402c95fd6d7847cc0f1777f11c7cce38
Tanmaykk <tanmaykachroo@gmail.com>
2021-07-21 19:15:22 +0530

    added graph

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,15 +2,15 @@ import { Flex, IconButton } from "@chakra-ui/react";
 import Link from "next/link";
 import { BiHomeAlt } from "react-icons/bi";
 import { GrGraphQl } from "react-icons/gr";
-import {GoCheck} from 'react-icons/go'
+import { GoCheck } from "react-icons/go";
 
 const Navbar = () => {
   return (
     <Flex
       flexDir={["row", "row", "column", "column"]}
       alignItems={["space-evenly", "space-evenly", "center", "center"]}
-      justifyContent='space-evenly'
-      py={['1rem','1rem','0','0']}
+      justifyContent="space-evenly"
+      py={["1rem", "1rem", "0", "0"]}
       h={["4rem", "4rem", "100vh", "100vh"]}
       w={["100%", "100%", "4rem", "4rem"]}
       bgColor="#010102"
@@ -21,13 +21,13 @@ const Navbar = () => {
       <Link href="/">
         <IconButton aria-label="Home" icon={<BiHomeAlt />} w="1rem" />
       </Link>
-      <Link href="/checklist">
-        <IconButton aria-label="Home" icon={<GoCheck />} w="1rem" />
-      </Link>
+
       <Link href="/graph">
         <IconButton aria-label="Home" icon={<GrGraphQl />} w="1rem" />
       </Link>
-      
+      <Link href="/checklist">
+        <IconButton aria-label="Home" icon={<GoCheck />} w="1rem" />
+      </Link>
     </Flex>
   );
 };
--- pages/graph.jsx
+++ pages/graph.jsx
@@ -1,7 +1,56 @@
+import { DataSet, Network } from "vis";
+import { useEffect, useRef } from "react";
+import { data } from "../data/network";
 const Graph = () => {
-  return <div>
-    [Graph will be added soon]
-  </div>;
+  const domNode = useRef(null);
+
+  // A reference to the vis network instance
+  const network = useRef(null);
+
+  const options = {
+    autoResize: true,
+    height: "500px",
+    width: "100%",
+    nodes: {
+      shape: "dot",
+      size: 12,
+      borderWidth: 1,
+      borderWidthSelected: 2,
+      brokenImage: undefined,
+      chosen: true,
+      font: {
+        color: "white",
+      },
+      color: {
+        border: "#909099",
+        background: "#909099",
+        highlight: {
+          border: "#white",
+          background: "#white",
+        },
+        hover: {
+          border: "#white",
+          background: "#white",
+        },
+      },
+    },
+    edges: {
+      color: {
+        color: "gray",
+      },
+    },
+  };
+
+  useEffect(() => {
+    network.current = new Network(domNode.current, data, options);
+  }, [domNode, network, data, options]);
+
+  return (
+    <>
+      <p>Try dragging the nodes. Currently in BETA mode.</p>
+      <div style={{ width: "100%", minHeight: "80vh" }} ref={domNode} />
+    </>
+  );
 };
 
 export default Graph;
