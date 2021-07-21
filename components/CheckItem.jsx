27eda2f28f41083c6c56a16289e017123bad292f
Tanmaykk <tanmaykachroo@gmail.com>
2021-07-21 15:45:50 +0530

    responsive checklist

--- components/CheckItem.jsx
+++ components/CheckItem.jsx
@@ -1,44 +1,62 @@
-import { Checkbox, Tag } from "@chakra-ui/react";
+import { Box, Checkbox, Tag } from "@chakra-ui/react";
 
-const CheckItem = ({ name, isChecked, id, changeCheck, date,type }) => {
-  
-  var tagColor ='teal'
-  switch(type){
-    case 'Sorting':
-      tagColor='red'
+const CheckItem = ({ name, isChecked, id, changeCheck, date, type }) => {
+  var tagColor = "teal";
+  switch (type) {
+    case "Sorting":
+      tagColor = "red";
       break;
-    case 'Searching':
-      tagColor='purple'
-      break;      
+    case "Searching":
+      tagColor = "purple";
+      break;
   }
   return (
-    <div style={{ display: "flex", alignItems: "center", margin: "1rem 1rem" }}>
-      <Checkbox
-        isChecked={isChecked}
-        onChange={() => {
-          changeCheck(id);
+    <>
+      <div
+        style={{
+          display: "flex",
+          width: "max-content",
+          alignItems: "center",
+          margin: "1rem 1rem",
+          position: "relative",
         }}
-        mr="1rem"
-      ></Checkbox>{" "}
-      {name}{" "}
-      {type ? (
-        <Tag
-          ml="1rem"
-          fontSize="0.7rem"
-          size="sm"
-          variant="solid"
-          colorScheme={tagColor}
+      >
+        <Checkbox
+          isChecked={isChecked}
+          onChange={() => {
+            changeCheck(id);
+          }}
+          mr="1rem"
+        ></Checkbox>{" "}
+        {name}{" "}
+        {type ? (
+          <Tag
+            ml="1rem"
+            fontSize="0.7rem"
+            size="sm"
+            variant="solid"
+            colorScheme={tagColor}
+          >
+            {type}
+          </Tag>
+        ) : (
+          ""
+        )}
+        <Box
+          position="absolute"
+          left="100%"
+          width="max-content"
+          // top='100%'
         >
-          {type}
-        </Tag>
-      ) : (
-        ""
-      )}
-      <span style={{ color: "gray", fontSize: "0.8rem", marginLeft: "1rem" }}>
-        {" "}
-        {isChecked && "completed on - " + date}
-      </span>
-    </div>
+          <span
+            style={{ color: "gray", fontSize: "0.8rem", marginLeft: "1rem" }}
+          >
+            {" "}
+            {isChecked && "completed: " + date}
+          </span>
+        </Box>
+      </div>
+    </>
   );
 };
 
--- pages/checklist.jsx
+++ pages/checklist.jsx
@@ -32,7 +32,7 @@ const CheckList = () => {
     }
   }, []);
   return (
-    <div style={{ margin: "0 auto", width: "80%" }}>
+    <div style={{ margin: "0 auto", width: "90%" }}>
       <Heading size="2xlg" mb="3rem">
         CheckList
       </Heading>
